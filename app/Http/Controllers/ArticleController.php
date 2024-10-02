924e646d8e57bc1e36fd3c925f50d4d1c25af336
Zain <supyaeh7@gmail.com>
2024-10-03 00:55:28 +0630

    updating MVC

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+// use Illuminate\Auth\Access\Gate;
 
 class ArticleController extends Controller
 {
@@ -49,6 +51,7 @@ class ArticleController extends Controller
       $article->title = request()->title;
       $article->body = request()->body;
       $article->category_id = request()->category_id;
+      $article ->user_id = auth()->user()->id;
       $article->save();
 
       return redirect('/articles');
@@ -56,8 +59,20 @@ class ArticleController extends Controller
 
    public function delete($id){
       $article = Article::find($id);
-      $article->delete();
 
-      return redirect('/articles')->with('info', 'Article Deleted');
+      if( Gate::allows('article-delete', $article)){
+         $article->delete();
+         return redirect('/articles')->with('info', 'Article Deleted');
+      }
+      else{
+         return redirect('/articles')->with('error', 'Unauthorize');
+     }
+     
+   }
+
+   // for authorization
+   public function __construct()
+   {
+      $this->middleware('auth')->except(['index', 'detail']);
    }
 }
--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,21 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class CommentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function create(){
         $comment = new Comment();
         $comment ->content = request()->content;
         $comment ->article_id = request()->article_id;
+        // $comment ->user_id = Auth::id();
+        $comment ->user_id = auth()->user()->id;
         $comment->save();
         
         return back();
     }
     public function delete($id) {
         $comment = Comment::find($id);
-        $comment->delete();
 
-        return back();
+        if(Gate::allows('comment-delete', $comment)){
+            $comment->delete();
+            return back();
+        }else{
+            return back()->with('error', 'Unauthorize');
+        }
+
     }
+
 }
